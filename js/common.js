061c4e8a9deb6543770e4a070f914f9d4f322852
TaTi <allaoleksyn@gmail.com>
2022-05-29 20:29:44 +0300

    daily Fahrenheit integration

--- js/common.js
+++ js/common.js
@@ -1,5 +1,3 @@
-let tempMetric = 0;
-let tempFeel = 0;
 let formInput = document.querySelector("#form-search");
 let inputTextCity = document.querySelector("#input-text");
 let cityName = document.querySelector("#name-city");
@@ -17,8 +15,37 @@ let tempTodayF = document.querySelector("#tempF-today");
 tempTodayF.innerHTML = `<snap class="temp-C-F-hover-js">°F</snap>`;
 let location1 = document.querySelector("#current-location");
 let imgHead = document.querySelector("#imgHead");
-let unitsFahrenheit = "imperial";
-let unitsCelcius = "metric";
+let units = "metric"; //"imperial";
+let sunTempMax = document.querySelector("#sun-temp-max");
+let sunTempMin = document.querySelector("#sun-temp-min");
+let monTempMax = document.querySelector("#mon-temp-max");
+let monTempMin = document.querySelector("#mon-temp-min");
+let tueTempMax = document.querySelector("#tue-temp-max");
+let tueTempMin = document.querySelector("#tue-temp-min");
+let wedTempMax = document.querySelector("#wed-temp-max");
+let wedTempMin = document.querySelector("#wed-temp-min");
+let thuTempMax = document.querySelector("#thu-temp-max");
+let thuTempMin = document.querySelector("#thu-temp-min");
+let friTempMax = document.querySelector("#fri-temp-max");
+let satTempMax = document.querySelector("#sat-temp-max");
+let satTempMin = document.querySelector("#sat-temp-min");
+let friTempMin = document.querySelector("#fri-temp-min");
+let tempMetric = null;
+let tempFeel = null;
+let sunTemperatureMax = null;
+let sunTemperatureMin = null;
+let monTemperatureMax = null;
+let monTemperatureMin = null;
+let tueTemperatureMax = null;
+let tueTemperatureMin = null;
+let wedTemperatureMax = null;
+let wedTemperatureMin = null;
+let thuTemperatureMax = null;
+let thuTemperatureMin = null;
+let friTemperatureMax = null;
+let friTemperatureMin = null;
+let satTemperatureMax = null;
+let satTemperatureMin = null;
 
 function displayForecastDaily(response) {
   let forecast = response.data.daily;
@@ -26,78 +53,78 @@ function displayForecastDaily(response) {
 
   let dateSun = new Date(response.data.daily[0].dt * 1000);
   let sunImgDay = document.querySelector("#imgsun");
-  let sunTempMax = document.querySelector("#sun-temp-max");
-  let sunTempMin = document.querySelector("#sun-temp-min");
   let sunDate = document.querySelector("#daySun");
   sunImgDay.setAttribute("src", `fotobank/${forecast[0].weather[0].icon}.svg`);
   sunTempMax.innerHTML = Math.round(forecast[0].temp.max);
   sunTempMin.innerHTML = ` ${Math.round(forecast[0].temp.min)}`;
   sunDate.innerHTML = `Sun, ${dateSun.getDate()}`;
+  sunTemperatureMax = Math.round(forecast[0].temp.max);
+  sunTemperatureMin = Math.round(forecast[0].temp.min);
 
   let dateMon = new Date(response.data.daily[1].dt * 1000);
   let monImgDay = document.querySelector("#imgmon");
-  let monTempMax = document.querySelector("#mon-temp-max");
-  let monTempMin = document.querySelector("#mon-temp-min");
   let monDate = document.querySelector("#dayMon");
   monImgDay.setAttribute("src", `fotobank/${forecast[1].weather[0].icon}.svg`);
   monTempMax.innerHTML = Math.round(forecast[1].temp.max);
   monTempMin.innerHTML = ` ${Math.round(forecast[1].temp.min)}`;
   monDate.innerHTML = `Mon, ${dateMon.getDate()}`;
+  monTemperatureMax = Math.round(forecast[1].temp.max);
+  monTemperatureMin = Math.round(forecast[1].temp.min);
 
   let dateTue = new Date(response.data.daily[2].dt * 1000);
   let tueImgDay = document.querySelector("#imgtue");
-  let tueTempMax = document.querySelector("#tue-temp-max");
-  let tueTempMin = document.querySelector("#tue-temp-min");
   let tueDate = document.querySelector("#dayTue");
   tueImgDay.setAttribute("src", `fotobank/${forecast[2].weather[0].icon}.svg`);
   tueTempMax.innerHTML = Math.round(forecast[2].temp.max);
   tueTempMin.innerHTML = ` ${Math.round(forecast[2].temp.min)}`;
   tueDate.innerHTML = `Tue, ${dateTue.getDate()}`;
+  tueTemperatureMax = Math.round(forecast[2].temp.max);
+  tueTemperatureMin = Math.round(forecast[2].temp.min);
 
   let dateWed = new Date(response.data.daily[3].dt * 1000);
   let wedImgDay = document.querySelector("#imgwed");
-  let wedTempMax = document.querySelector("#wed-temp-max");
-  let wedTempMin = document.querySelector("#wed-temp-min");
   let wedDate = document.querySelector("#dayWed");
   wedImgDay.setAttribute("src", `fotobank/${forecast[3].weather[0].icon}.svg`);
   wedTempMax.innerHTML = Math.round(forecast[3].temp.max);
   wedTempMin.innerHTML = ` ${Math.round(forecast[3].temp.min)}`;
   wedDate.innerHTML = `Wed, ${dateWed.getDate()}`;
+  wedTemperatureMax = Math.round(forecast[3].temp.max);
+  wedTemperatureMin = Math.round(forecast[3].temp.min);
 
   let dateThu = new Date(response.data.daily[4].dt * 1000);
   let thuImgDay = document.querySelector("#imgthu");
-  let thuTempMax = document.querySelector("#thu-temp-max");
-  let thuTempMin = document.querySelector("#thu-temp-min");
   let thuDate = document.querySelector("#dayThu");
   thuImgDay.setAttribute("src", `fotobank/${forecast[4].weather[0].icon}.svg`);
   thuTempMax.innerHTML = Math.round(forecast[4].temp.max);
   thuTempMin.innerHTML = ` ${Math.round(forecast[4].temp.min)}`;
   thuDate.innerHTML = `Thu, ${dateThu.getDate()}`;
+  thuTemperatureMax = Math.round(forecast[4].temp.max);
+  thuTemperatureMin = Math.round(forecast[4].temp.min);
 
   let dateFri = new Date(response.data.daily[5].dt * 1000);
   let friImgDay = document.querySelector("#imgfri");
-  let friTempMax = document.querySelector("#fri-temp-max");
-  let friTempMin = document.querySelector("#fri-temp-min");
   let friDate = document.querySelector("#dayFri");
   friImgDay.setAttribute("src", `fotobank/${forecast[5].weather[0].icon}.svg`);
   friTempMax.innerHTML = Math.round(forecast[5].temp.max);
   friTempMin.innerHTML = ` ${Math.round(forecast[5].temp.min)}`;
   friDate.innerHTML = `Fri, ${dateFri.getDate()}`;
+  friTemperatureMax = Math.round(forecast[5].temp.max);
+  friTemperatureMin = Math.round(forecast[5].temp.min);
 
   let dateSat = new Date(response.data.daily[6].dt * 1000);
   let satImgDay = document.querySelector("#imgsat");
-  let satTempMax = document.querySelector("#sat-temp-max");
-  let satTempMin = document.querySelector("#sat-temp-min");
   let satDate = document.querySelector("#daySat");
   satImgDay.setAttribute("src", `fotobank/${forecast[6].weather[0].icon}.svg`);
   satTempMax.innerHTML = Math.round(forecast[6].temp.max);
   satTempMin.innerHTML = ` ${Math.round(forecast[6].temp.min)}`;
   satDate.innerHTML = `Sat, ${dateSat.getDate()}`;
+  satTemperatureMax = Math.round(forecast[6].temp.max);
+  satTemperatureMin = Math.round(forecast[6].temp.min);
 }
 
 function getForecastCoordDaily(coordinates) {
   let apiKey = `910507594fe3cf58151f5b16db41c101`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayForecastDaily);
   console.log(apiUrl);
 }
@@ -136,7 +163,7 @@ function showCityWeather(response) {
 
 function searchCity(city) {
   let apiKey = `910507594fe3cf58151f5b16db41c101`;
-  let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios
     .get(cityUrl)
     .then(showCityWeather)
@@ -163,6 +190,21 @@ function lookTempC() {
   tempTodayC.classList.remove("active-href");
   tempTodayF.classList.add("active-href");
   tempTodayC.innerHTML = `<snap>°C</snap>`;
+
+  sunTempMax.innerHTML = Math.round(sunTemperatureMax);
+  sunTempMin.innerHTML = ` ${Math.round(sunTemperatureMin)}`;
+  monTempMax.innerHTML = Math.round(monTemperatureMax);
+  monTempMin.innerHTML = ` ${Math.round(monTemperatureMin)}`;
+  tueTempMax.innerHTML = Math.round(tueTemperatureMax);
+  tueTempMin.innerHTML = ` ${Math.round(tueTemperatureMin)}`;
+  wedTempMax.innerHTML = Math.round(wedTemperatureMax);
+  wedTempMin.innerHTML = ` ${Math.round(wedTemperatureMin)}`;
+  thuTempMax.innerHTML = Math.round(thuTemperatureMax);
+  thuTempMin.innerHTML = ` ${Math.round(thuTemperatureMin)}`;
+  friTempMax.innerHTML = Math.round(friTemperatureMax);
+  friTempMin.innerHTML = ` ${Math.round(friTemperatureMin)}`;
+  satTempMax.innerHTML = Math.round(satTemperatureMax);
+  satTempMin.innerHTML = ` ${Math.round(satTemperatureMin)}`;
 }
 
 function lookTempF() {
@@ -179,6 +221,21 @@ function lookTempF() {
   tempTodayF.innerHTML = `°F`;
   tempTodayC.classList.add("active-href");
   tempTodayF.classList.remove("active-href");
+
+  sunTempMax.innerHTML = Math.round((sunTemperatureMax * 9) / 5 + 32);
+  sunTempMin.innerHTML = ` ${Math.round((sunTemperatureMin * 9) / 5 + 32)}`;
+  monTempMax.innerHTML = Math.round((monTemperatureMax * 9) / 5 + 32);
+  monTempMin.innerHTML = ` ${Math.round((monTemperatureMin * 9) / 5 + 32)}`;
+  tueTempMax.innerHTML = Math.round((tueTemperatureMax * 9) / 5 + 32);
+  tueTempMin.innerHTML = ` ${Math.round((tueTemperatureMin * 9) / 5 + 32)}`;
+  wedTempMax.innerHTML = Math.round((wedTemperatureMax * 9) / 5 + 32);
+  wedTempMin.innerHTML = ` ${Math.round((wedTemperatureMin * 9) / 5 + 32)}`;
+  thuTempMax.innerHTML = Math.round((thuTemperatureMax * 9) / 5 + 32);
+  thuTempMin.innerHTML = ` ${Math.round((thuTemperatureMin * 9) / 5 + 32)}`;
+  friTempMax.innerHTML = Math.round((friTemperatureMax * 9) / 5 + 32);
+  friTempMin.innerHTML = ` ${Math.round((friTemperatureMin * 9) / 5 + 32)}`;
+  satTempMax.innerHTML = Math.round((satTemperatureMax * 9) / 5 + 32);
+  satTempMin.innerHTML = ` ${Math.round((satTemperatureMin * 9) / 5 + 32)}`;
 }
 
 function getLocation() {
@@ -189,8 +246,11 @@ function showCityLocation(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let apiKey = `910507594fe3cf58151f5b16db41c101`;
-  let cityUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  let cityUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
   axios.get(cityUrl).then(showCityWeather);
+  console.log(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`
+  );
 }
 
 function getActiveDay() {
